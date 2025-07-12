15c54da9fc91a60f358d430cfdc3900c0d474b26
root <root@server.sajbersfera.in.rs>
2025-07-12 09:51:54 +0200

    Initial import of Newspaper parent theme

---
+++ includes/wp-booster/tagdiv-global.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * Theme globals class
+ * Here we store the global state of the theme. All globals are here
+ */
+class tagdiv_global {
+
+	/**
+	 * theme plugins
+	 * 'PLUGIN_CONSTANT' => 'hash'
+	 * @var array
+	 */
+	private static $td_plugins = array(
+		'TD_COMPOSER'       => array( 'version' => '492e3cf54bd42df3b24a3de8e2307cafx',         'class' => 'tdc_version_check' ),
+		'TD_CLOUD_LIBRARY'  => array( 'version' => 'd578089f160957352b9b4ca6d880fd8f',    'class' => 'tdb_version_check' ),
+		'TD_SOCIAL_COUNTER' => array( 'version' => '0f1656c48c5da80d87e8e2dbbe8b195b',   'class' => 'td_social_counter_plugin' ),
+		'TD_NEWSLETTER'     => array( 'version' => '795e74d53223156068c4e29e39e0ed3e',       'class' => 'td_newsletter_version_check' ),
+		'TD_SUBSCRIPTION'   => array( 'version' => '___td-subscription___',     'class' => 'tds_version_check' ),
+		'TD_MOBILE_PLUGIN'  => array( 'version' => 'cca29e6e1f21dd83ab5a5f38860d1fd9',    'class' => 'td_mobile_theme' ),
+		'AMP'               => array( 'version' => '___amp___',                 'class' => 'AMP_Autoloader' ),
+		'TD_STANDARD_PACK'  => array( 'version' => '8ae1459a1d876d507918f73cef68310e',    'class' => 'tdsp_version_check' ),
+		'TD_WOO'            => array( 'version' => '2852a64b55307dfd7353b7b76ce6ba63',              'class' => 'td_woo_version_check' )
+	);
+
+
+	/**
+	 * Get the $td_plugins hashes array
+	 * @return array
+	 */
+	static function get_td_plugins() {
+		return self::$td_plugins;
+	}
+
+	/**
+	 * set below with either http or https string
+	 * @var string
+	 */
+    static $http_or_https = 'http';
+
+	/**
+	 * Determines if SSL is used and sets the $http_or_https global
+	 */
+    static function set_http_or_https() {
+	    if ( is_ssl() ) {
+		    self::$http_or_https = 'https';
+	    }
+    }
+
+	/**
+	 * the plugins that are installable via the theme > plugins panel & tgma
+	 * @var array
+	 */
+    static $theme_plugins_list = array();
+
+	/**
+	 * the plugins that are just for information proposes
+	 * @var array
+	 */
+	static $theme_plugins_for_info_list = array();
+
+
+    /**
+     * the js files that are used in wp-admin
+     * @var array
+     *
+     * @todo check what js files are needed for wp admin
+     */
+    static $js_files_for_wp_admin = array (
+        'td_wp_admin'     => '/includes/wp-booster/wp-admin/js/td_wp_admin.js',
+        'td_edit_page'    => '/includes/wp-booster/wp-admin/js/td_edit_page.js',
+        'td_page_options' => '/includes/wp-booster/wp-admin/js/td_page_options.js',
+        'td_tooltip'      => '/includes/wp-booster/wp-admin/js/tooltip.js',
+	    'td_confirm'      => '/includes/wp-booster/wp-admin/js/tdConfirm.js',
+    );
+
+}
+
+/**
+ * set http or https
+ */
+tagdiv_global::set_http_or_https();
+
+
---
+++ purgecss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: [
+    './wp-content/themes/Newspaper/**/*.php',
+    './wp-content/plugins/td-composer/mobile/**/*.php'
+  ],
+  css: ['./wp-content/plugins/td-composer/mobile/assets/css/*.css'],
+  output: './purged-mobile-css/'
+};
